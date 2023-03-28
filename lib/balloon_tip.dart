084ca43b390183ba0b22160b33a807980d2e7e20
joe <jomonjoshygeorge@gmail.com>
2023-03-28 18:20:36 +0530

    added teamplate exmaple project and skeleton files

--- lib/balloon_tip.dart
+++ lib/balloon_tip.dart
@@ -1,7 +1,5 @@
 library balloon_tip;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/models/index.dart';
+export 'src/utils/index.dart';
+export 'src/views/index.dart';
